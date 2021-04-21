6da1b83299dc67e0b1ea646b408ed4ff10f99503
Muab-Dib <jpsteph1@asu.edu>
2021-04-21 11:46:13 -0700

    These are my implementations of the manager classes thus far. This will be modified in the near future of course to make it compatible with future changes.

--- server/src/main/java/spring/manager/OrderManager.java
+++ server/src/main/java/spring/manager/OrderManager.java
@@ -2,6 +2,9 @@ package spring.manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import spring.entity.Order;
+import spring.model.ItemInfo;
+import spring.model.ShoppingCartInfo;
 import spring.repositories.OrderRepository;
 import spring.repositories.PendingOrderRepository;
 
@@ -20,8 +23,31 @@ public class OrderManager {
         this.pendingOrderRepository = pendingOrderRepository;
     }
 
+    /*private Order upsert(Long orderId, int quantity, Long itemid)
+    {
 
-    // TODO save Order
+    }*/
 
+    private Long saveToOrder(ItemInfo itemInfo)
+    {
+        //TODO validate shopping cart info.
+        //if(!orderRepository.existsById(saveToOrder(itemInfo)))
+            //orderRepository.upsert(orderId, quantity, itemid);
 
+        //TODO create order from valid shopping cart info
+
+        Order order = new Order();
+        order = orderRepository.save(order);
+        return order.getOrderNumber();
+    }
+
+    /*public Long saveToCart(ShoppingCartInfo shoppingCartInfo)
+    {
+
+    }
+
+    public ShoppingCartInfo loadCart(String username)
+    {
+
+    }*/
 }
