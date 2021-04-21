5b78868a222b4f307ac7f2cabba8e1744ed84720
Muab-Dib <jpsteph1@asu.edu>
2021-04-21 14:57:06 -0700

    Some additions to OrderManager, including a saveOrder method, if statement needs to be finished.

--- server/src/main/java/spring/manager/OrderManager.java
+++ server/src/main/java/spring/manager/OrderManager.java
@@ -3,7 +3,10 @@ package spring.manager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.entity.Order;
-import spring.model.ItemInfo;
+import spring.entity.PendingOrder;
+import spring.model.OrderInfo;
+import java.math.BigInteger;
+import java.util.Optional;
 import spring.model.ShoppingCartInfo;
 import spring.repositories.OrderRepository;
 import spring.repositories.PendingOrderRepository;
@@ -23,31 +26,50 @@ public class OrderManager {
         this.pendingOrderRepository = pendingOrderRepository;
     }
 
-    /*private Order upsert(Long orderId, int quantity, Long itemid)
+    private Optional<Order> update(String username, BigInteger itemID, Integer quantity)
     {
+        return (orderRepository.update(username, itemID, quantity));
+    }
 
-    }*/
-
-    private Long saveToOrder(ItemInfo itemInfo)
+    private OrderInfo saveToOrder(OrderInfo itemInfo)
     {
         //TODO validate shopping cart info.
-        //if(!orderRepository.existsById(saveToOrder(itemInfo)))
-            //orderRepository.upsert(orderId, quantity, itemid);
+//        if(!orderRepository.existsById(saveToOrder(itemInfo)))
+//            orderRepository.update(, quantity, itemid);
 
         //TODO create order from valid shopping cart info
+        if(!orderRepository.existsById(itemInfo.getId().longValue()))
+        {
 
+        }
         Order order = new Order();
         order = orderRepository.save(order);
-        return order.getOrderNumber();
+        return itemInfo;
+    }
+    /*
+    This method is to save an order rather than saving an item to an order. So we're getting the order itself and
+    saving that potential order. To do this, PendingOrder is of course needed as PendingOrder basically is an order
+    that is pending if it is valid or invalid. So we need a PendingOrder object, and save that order into a pending order
+    repository.
+     */
+    private PendingOrder saveOrder(Order item)
+    {
+//        if(item.getItemId())
+//        {
+//
+//        }
+        PendingOrder pendOrder = new PendingOrder();
+        pendOrder.setOrder(item);
+        return pendOrder;
     }
 
-    /*public Long saveToCart(ShoppingCartInfo shoppingCartInfo)
-    {
-
-    }
-
-    public ShoppingCartInfo loadCart(String username)
-    {
-
-    }*/
+//    public Long saveToCart(ShoppingCartInfo shoppingCartInfo)
+//    {
+//
+//    }
+//
+//    public ShoppingCartInfo loadCart(String username)
+//    {
+//
+//    }
 }
